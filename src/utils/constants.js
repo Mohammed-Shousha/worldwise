068bbd6233e2ce1df35982eb88c5284558309a76
Mohammed Shousha <mshousha29@gmail.com>
2024-09-07 17:38:59 +0300

    Add constants for server URL, city API URL, and user avatar URL

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const SERVER_URL = 'http://localhost:9000';
+
+export const CITY_API_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
+
+export const USER_AVATAR_URL = 'https://i.pravatar.cc/100?u=zz'
